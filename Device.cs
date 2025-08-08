a0dca4e948f28f6a5502cd95a7f93c30f381eee3
NurSyahirahRahim <125422226+NurSyahirahRahim@users.noreply.github.com>
2025-08-08 10:50:52 +0800

    Update Device.cs

--- Device.cs
+++ Device.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Data;
 
 namespace IoT_Device_Management
 {
@@ -13,5 +11,66 @@ namespace IoT_Device_Management
         public string DeviceType { get; set; }
         public string Status { get; set; }
         public DateTime LastActive { get; set; }
+    }
+}
+
+public class ItemVM : IEditableObject, INotifyPropertyChanged
+{
+    event PropertyChangedEventHandler? INotifyPropertyChanged.PropertyChanged
+    {
+        add
+        {
+            throw new NotImplementedException();
+        }
+
+        remove
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    void IEditableObject.BeginEdit()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IEditableObject.CancelEdit()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IEditableObject.EndEdit()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+
+public class ModuleVM : INotifyPropertyChanged
+{
+    ICollectionView Items { get; }
+
+    public ModuleVM(ObservableCollection<ItemVM> items)
+    {
+        Items = CollectionViewSource.GetDefaultView(items);
+    }
+
+    event PropertyChangedEventHandler? INotifyPropertyChanged.PropertyChanged
+    {
+        add
+        {
+            throw new NotImplementedException();
+        }
+
+        remove
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public void RefreshSafely()
+    {
+        ((IEditableCollectionView)Items).CommitEdit(); 
+        Items.Refresh();
     }
 }
